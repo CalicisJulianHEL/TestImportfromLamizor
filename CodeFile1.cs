62b5ad9f64685ce1353de1f61d5c91568c081da3
Lamizor Prod <lamizor.prod@gmail.com>
2022-10-05 08:28:44 +0200

    Création du programme

---
+++ CodeFile1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestImportFromLamizor
+{
+  internal static class Program
+  {
+    private static void Main()
+    {
+      Console.WriteLine("Coucou Programme");
+      Console.ReadLine();
+    }
+  }
+}
